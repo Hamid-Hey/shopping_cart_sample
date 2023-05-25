a7c95ed9e6e00c93dce312476141753bc3990156
Hamid Heydari <hamid6885@gmail.com>
2023-05-25 17:02:27 +0430

    feat: add VCard component

---
+++ components/VCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div :class="['v-card', { 'v-card--outline': outline }]">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VCard',
+
+  props: {
+    outline: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.v-card {
+  font-family: IRANYekan, 'Segoe UI', tahoma, Verdana, Arial, sans-serif !important;
+  padding: 12px 20px;
+  background-color: #f8f8f8;
+  border: 1px solid #e0e0e6;
+  border-radius: 10px;
+  min-width: 280px;
+  direction: rtl;
+
+  &--outline {
+    background-color: unset;
+    min-width: unset;
+    padding: 6px 12px;
+    font-size: 12px;
+    font-weight: 700;
+    color: #555555;
+  }
+}
+</style>
